228653d7a4b15fd7e367ca957ba618e82686144f
RHAYMISON  CRISTIAN <rhaymison@hotmail.com>
2023-04-02 16:34:03 -0300

    inital-globo-get

--- src/traits/ClearXmlArray.php
+++ src/traits/ClearXmlArray.php
@@ -48,4 +48,45 @@ trait ClearXmlArray
         }
         return $newArray;
     }
+
+    /**
+     * this method get xml brute globo  and get the main 
+     * parts to send to destinantion
+     * @param array $financialArray
+     * @return array
+     */
+    public function globoEntretainClear(array $globoEntretain): array
+    {
+        (array) $newArray = [];
+        if (isset($globoEntretain['channel']) && count($globoEntretain['channel']['item']) > 0) {
+            foreach ($globoEntretain['channel']['item'] as $new) {
+                $imgDesc = $this->getImgInsideString($new['description']);
+                array_push($newArray, [
+                    'url' => $new['link'],
+                    'title' => $new['title'],
+                    'description' => $imgDesc[1],
+                    'image' => $imgDesc[0],
+                ]);
+            }
+        }
+        return $newArray;
+    }
+
+    /**
+     * This methodo clean a img description of globo, cause (i dont know) they 
+     * return a img tag inside a description tag..????????
+     * The best way to provider this clean in front is that (at this moment)
+     * @param string $descrption;
+     * @retrn array
+     */
+    public function getImgInsideString(string $descrption): array
+    {
+        $imgFinder = explode("<br />", $descrption);
+        $img = trim(str_replace('/>', '', str_replace('"', '', explode("src=", explode("<br />", $imgFinder[0])[0])[1])));
+        $descrption = trim($imgFinder[1]);
+        return [
+            $img,
+            $descrption
+        ];
+    }
 }
